8446a3a9c66899acad93d49513b073a0fd21b85f
Saif Khan <sj5khan@uwaterloo.ca>
2023-05-01 21:06:03 -0400

    Initial Commit

---
+++ AiLiteracyTeacher/AiLiteracyTeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiLiteracyTeacherApp.swift
+//  AiLiteracyTeacher
+//
+//  Created by Saif Khan on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct AiLiteracyTeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
