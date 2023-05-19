f638e096063b94fd01c9b467dcf12dbbe73a7a21
Hassan Khan <hassanjkhan6@gmail.com>
2023-05-18 23:41:49 -0400

    Adding remaining views to match the figma

--- AILiteracyTeacherUIKit/AILiteracyTeacher/AILiteracyTeacher/WordSelectionView.swift
+++ AILiteracyTeacherUIKit/AILiteracyTeacher/AILiteracyTeacher/WordSelectionView.swift
@@ -11,11 +11,14 @@ import UIKit
 class WordSelectionView: UIView {
     
     // constants
-    let smallPadding = 4.0
-    let mediumPadding = 15.0
-    let firstStoryButtonHeight = 40.0
+    let extraSmallPadding = 6.0
+    let smallPadding = 10.0
+    let mediumPadding = 24.0
+    let textFieldHeight = 40.0
+    let textFieldWidth = 84.0
     let contentViewHeight = 400.0
     let contentViewWidth = 400.0
+    let createButtonPadding = 60.0
     
     lazy var contentView: UIImageView = {
       let contentView = UIImageView()
@@ -35,20 +38,21 @@ class WordSelectionView: UIView {
         return firstStoryLabel
     }()
     
-    let firstStoryButton: UIButton = {
-        let firstStoryButton = UIButton()
-        firstStoryButton.setTitle("space girl", for: .normal)
-        firstStoryButton.backgroundColor = UIColor.orange
-        firstStoryButton.setTitleColor(UIColor.white, for: .normal)
-        firstStoryButton.layer.cornerRadius = 8
-        firstStoryButton.clipsToBounds = true
-        firstStoryButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        return firstStoryButton
+    let firstStoryTextField: UITextField = {
+        let firstStoryTextField = UITextField()
+        firstStoryTextField.backgroundColor = UIColor.orange
+        firstStoryTextField.textColor = UIColor.white
+        firstStoryTextField.attributedPlaceholder = NSAttributedString(string: "space girl", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        firstStoryTextField.textAlignment = .center
+        firstStoryTextField.layer.cornerRadius = 8.0
+        firstStoryTextField.clipsToBounds = true
+        firstStoryTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return firstStoryTextField
     }()
     
     lazy var secondStoryLabel: UILabel = {
-        let secondStoryLabel = UILabel()//frame: CGRect(x: 0, y: 0, width: 100, height: 20))
+        let secondStoryLabel = UILabel()
         secondStoryLabel.font = UIFont.systemFont(ofSize: 22, weight: .medium)
         secondStoryLabel.text = "in a"
         secondStoryLabel.textAlignment = .center
@@ -56,23 +60,106 @@ class WordSelectionView: UIView {
 
         return secondStoryLabel
     }()
-//    let secondStoryButton: UIButton
-//
-//    let firstDescriptiveLabel: UILabel
-//    let firstDescriptiveButton: UIButton
-//    let secondDescriptiveButton: UIButton
-//    let secondDescriptiveLabel: UILabel
-//    let thirdDescriptiveButton: UIButton
-//
-//    let createButton: UIButton
+    
+    let secondStoryTextField: UITextField = {
+        let secondStoryTextField = UITextField()
+        secondStoryTextField.backgroundColor = UIColor.orange
+        secondStoryTextField.textColor = UIColor.white
+        secondStoryTextField.attributedPlaceholder = NSAttributedString(string: "place", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        secondStoryTextField.textAlignment = .center
+        secondStoryTextField.layer.cornerRadius = 8.0
+        secondStoryTextField.clipsToBounds = true
+        secondStoryTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return secondStoryTextField
+    }()
+
+    let firstDescriptiveLabel: UILabel = {
+        let firstDescriptiveLabel = UILabel()
+        firstDescriptiveLabel.font = UIFont.systemFont(ofSize: 22, weight: .medium)
+        firstDescriptiveLabel.text = "Space girl is"
+        firstDescriptiveLabel.textAlignment = .center
+        firstDescriptiveLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        return firstDescriptiveLabel
+    }()
+    
+    let firstDescriptiveTextField: UITextField = {
+        let firstDescriptiveTextField = UITextField()
+        firstDescriptiveTextField.backgroundColor = UIColor.orange
+        firstDescriptiveTextField.textColor = UIColor.white
+        firstDescriptiveTextField.attributedPlaceholder = NSAttributedString(string: "brave", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        firstDescriptiveTextField.textAlignment = .center
+        firstDescriptiveTextField.layer.cornerRadius = 8.0
+        firstDescriptiveTextField.clipsToBounds = true
+        firstDescriptiveTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return firstDescriptiveTextField
+    }()
+    
+    let secondDescriptiveTextField: UITextField = {
+        let secondDescriptiveTextField = UITextField()
+        secondDescriptiveTextField.backgroundColor = UIColor.orange
+        secondDescriptiveTextField.textColor = UIColor.white
+        secondDescriptiveTextField.attributedPlaceholder = NSAttributedString(string: "funny", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        secondDescriptiveTextField.textAlignment = .center
+        secondDescriptiveTextField.layer.cornerRadius = 8.0
+        secondDescriptiveTextField.clipsToBounds = true
+        secondDescriptiveTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return secondDescriptiveTextField
+    }()
+    
+    let secondDescriptiveLabel: UILabel = {
+        let secondDescriptiveLabel = UILabel()
+        secondDescriptiveLabel.font = UIFont.systemFont(ofSize: 22, weight: .medium)
+        secondDescriptiveLabel.text = "and"
+        secondDescriptiveLabel.textAlignment = .center
+        secondDescriptiveLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        return secondDescriptiveLabel
+    }()
+    
+    let thirdDescriptiveTextField: UITextField = {
+        let thirdDescriptiveTextField = UITextField()
+        thirdDescriptiveTextField.backgroundColor = UIColor.orange
+        thirdDescriptiveTextField.textColor = UIColor.white
+        thirdDescriptiveTextField.attributedPlaceholder = NSAttributedString(string: "smart", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        thirdDescriptiveTextField.textAlignment = .center
+        thirdDescriptiveTextField.layer.cornerRadius = 8.0
+        thirdDescriptiveTextField.clipsToBounds = true
+        thirdDescriptiveTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return thirdDescriptiveTextField
+    }()
+    
+    let createButton: UIButton = {
+        let createButton = UIButton()
+        createButton.setTitle("CREATE", for: .normal)
+        createButton.setTitleColor(UIColor.white, for: .normal)
+        createButton.backgroundColor = UIColor.black
+        createButton.layer.cornerRadius = 8.0
+        createButton.clipsToBounds = true
+        createButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        return createButton
+    }()
 
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(contentView)
         addSubview(firstStoryLabel)
-        addSubview(firstStoryButton)
+        addSubview(firstStoryTextField)
         addSubview(secondStoryLabel)
+        addSubview(secondStoryTextField)
+        addSubview(firstDescriptiveLabel)
+        addSubview(firstDescriptiveTextField)
+        addSubview(secondDescriptiveTextField)
+        addSubview(secondDescriptiveLabel)
+        addSubview(thirdDescriptiveTextField)
+        addSubview(createButton)
+        
         self.backgroundColor = UIColor.white
         layoutSubviews()
     }
@@ -85,24 +172,62 @@ class WordSelectionView: UIView {
     override func layoutSubviews() {
         
         NSLayoutConstraint.activate([
-            //layout contentView
+            // layout contentView
             contentView.topAnchor.constraint(equalTo: topAnchor, constant: mediumPadding),
             contentView.heightAnchor.constraint(equalToConstant: contentViewHeight),
             contentView.centerXAnchor.constraint(equalTo: centerXAnchor),
             contentView.widthAnchor.constraint(equalToConstant: contentViewWidth),
               
-            //layout firstStoryLabel
+            // layout firstStoryLabel
             firstStoryLabel.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: mediumPadding),
             firstStoryLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: mediumPadding),
               
-            //layout firstStoryButton
-            firstStoryButton.centerYAnchor.constraint(equalTo: firstStoryLabel.centerYAnchor),
-            firstStoryButton.leadingAnchor.constraint(equalTo: firstStoryLabel.trailingAnchor, constant: smallPadding),
-            firstStoryButton.heightAnchor.constraint(equalToConstant: firstStoryButtonHeight),
+            // layout firstStoryTextField
+            firstStoryTextField.centerYAnchor.constraint(equalTo: firstStoryLabel.centerYAnchor),
+            firstStoryTextField.leadingAnchor.constraint(equalTo: firstStoryLabel.trailingAnchor, constant: extraSmallPadding),
+            firstStoryTextField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            firstStoryTextField.widthAnchor.constraint(equalToConstant: textFieldWidth),
               
-            //layout secondStoryLabel
+            // layout secondStoryLabel
             secondStoryLabel.topAnchor.constraint(equalTo: firstStoryLabel.bottomAnchor, constant: smallPadding),
-            secondStoryLabel.leadingAnchor.constraint(equalTo: firstStoryLabel.leadingAnchor)
+            secondStoryLabel.leadingAnchor.constraint(equalTo: firstStoryLabel.leadingAnchor),
+            
+            // layout secondStoryTextField
+            secondStoryTextField.centerYAnchor.constraint(equalTo: secondStoryLabel.centerYAnchor),
+            secondStoryTextField.leadingAnchor.constraint(equalTo: secondStoryLabel.trailingAnchor, constant: extraSmallPadding),
+            secondStoryTextField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            secondStoryTextField.widthAnchor.constraint(equalToConstant: textFieldWidth),
+            
+            // layout firstDescriptiveLabel
+            firstDescriptiveLabel.topAnchor.constraint(equalTo: secondStoryLabel.bottomAnchor, constant: mediumPadding),
+            firstDescriptiveLabel.leadingAnchor.constraint(equalTo: secondStoryLabel.leadingAnchor),
+            
+            // layout firstDescriptiveTextField
+            firstDescriptiveTextField.centerYAnchor.constraint(equalTo: firstDescriptiveLabel.centerYAnchor),
+            firstDescriptiveTextField.leadingAnchor.constraint(equalTo: firstDescriptiveLabel.trailingAnchor, constant: extraSmallPadding),
+            firstDescriptiveTextField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            firstDescriptiveTextField.widthAnchor.constraint(equalToConstant: textFieldWidth),
+            
+            // layout secondDescriptiveTextField
+            secondDescriptiveTextField.centerYAnchor.constraint(equalTo: firstDescriptiveTextField.centerYAnchor),
+            secondDescriptiveTextField.leadingAnchor.constraint(equalTo: firstDescriptiveTextField.trailingAnchor, constant: extraSmallPadding),
+            secondDescriptiveTextField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            secondDescriptiveTextField.widthAnchor.constraint(equalToConstant: textFieldWidth),
+            
+            // layout secondDescriptiveLabel
+            secondDescriptiveLabel.topAnchor.constraint(equalTo: firstDescriptiveLabel.bottomAnchor, constant: mediumPadding),
+            secondDescriptiveLabel.leadingAnchor.constraint(equalTo: firstDescriptiveLabel.leadingAnchor),
+            
+            // layout thirdDescriptiveTextField
+            thirdDescriptiveTextField.centerYAnchor.constraint(equalTo: secondDescriptiveLabel.centerYAnchor),
+            thirdDescriptiveTextField.leadingAnchor.constraint(equalTo: secondDescriptiveLabel.trailingAnchor, constant: extraSmallPadding),
+            thirdDescriptiveTextField.heightAnchor.constraint(equalToConstant: textFieldHeight),
+            thirdDescriptiveTextField.widthAnchor.constraint(equalToConstant: textFieldWidth),
+            
+            // layout createButton
+            createButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            createButton.topAnchor.constraint(equalTo: thirdDescriptiveTextField.bottomAnchor, constant:createButtonPadding),
+            createButton.widthAnchor.constraint(equalToConstant: textFieldWidth)
             ])
     }
     
